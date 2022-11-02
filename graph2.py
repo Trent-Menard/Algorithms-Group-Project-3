36f3ce74af07bb54fdeea99705643dbebf70648e
DJMack2000 <dmcallister@cub.uca.edu>
2022-11-01 21:43:12 -0500

    forgot source
    Sourced me code and made it easier to read

--- graph2.py
+++ graph2.py
@@ -2,20 +2,33 @@
 """
 Created on Tue Nov  1 21:33:32 2022
 
-@author: DJMack
+@author: Dallas McAllister
 """
+#source code
+#https://www.youtube.com/watch?v=xREnpVUbkFI
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
 G = nx.DiGraph()
+#setting the edges of the graph
 G.add_edges_from([ ('4', '12'), ('4', '1'), ('1', '3')])
+
+#the inner edges of the graph
 G.add_edges_from([ ('4', '2'), ('2', '1'), ('3', '2')])
+
+#the outer edges of the graph
 G.add_edges_from([ ('3', '5'), ('5', '6'), ('6', '7')])
+
+#the inner edges of the graph
 G.add_edges_from([ ('5', '8'), ('6', '8'), ('8', '10')])
 G.add_edges_from([ ('8', '9'), ('9', '5'), ('9', '11')])
+
+#the outer edges of the graph
 G.add_edges_from([ ('7', '10'), ('10', '11'), ('11', '12')])
+
+#displaying the graph
 pos = nx.spring_layout(G)
 nx.draw_networkx_nodes(G, pos, node_size=400)
 nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
